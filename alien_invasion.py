cec7e37f2029c06339bf000c44e021c531718b7f
iconoclast// <intergalacticamour@gmail.com>
2023-07-09 21:33:13 -0400

    Added the ship image.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,16 +2,25 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Manage assets and behavior"""
 
     def __init__(self):
         """Initialize the game, create game resources"""
         pygame.init()
-
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
-
+        
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+    
     def run_game(self):
         """Main game loop"""
         while True:
@@ -20,8 +29,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Make a game instance and run it.
